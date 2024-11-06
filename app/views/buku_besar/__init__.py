3f15aec3e39b59600b9d5b1e0a39bdda212548d2
hdinjos <hdinjos@gmail.com>
2024-11-06 17:53:36 +0700

    buku besar done

--- app/views/buku_besar/__init__.py
+++ app/views/buku_besar/__init__.py
@@ -1,15 +1,36 @@
 from app import server,db
 from flask import render_template, request, redirect, url_for
 from app.models.DaftarAkun import DaftarAkun
+from app.models.JurnalUmum import JurnalUmum
 import random
 import string
+from itertools import groupby
 
 
 
 @server.route("/buku-besar")
 def index_buku():
-    daftar_akuns = DaftarAkun.query.order_by(DaftarAkun.id.desc())
-    return render_template("buku_besar/index.html", title="Daftar Akun", daftar_akuns=daftar_akuns)
+    # daftar_akuns = DaftarAkun.query.order_by(DaftarAkun.id.desc())
+    jurnal_umums = db.session.query( JurnalUmum.id, JurnalUmum.date, JurnalUmum.date,JurnalUmum.note,JurnalUmum.debit,JurnalUmum.credit, DaftarAkun.id.label('daftar_akun_id'), DaftarAkun.name.label('daftar_akun_name'), DaftarAkun.code.label('code')).filter(JurnalUmum.daftar_akun_id == DaftarAkun.id).all()
+    
+    # Step 1: Sort the list by 'by' field to ensure groupby works properly
+    sorted_lists = sorted(jurnal_umums, key=lambda x: x.daftar_akun_name)
+
+    # Step 2: Group the sorted list by 'by' field
+    grouped = groupby(sorted_lists, key=lambda x: x.daftar_akun_name +" (" + x.code +")")
+
+    # # Step 3: Format the result
+    datas = []
+    for key, group in grouped:
+        ingroup = []
+        datas.append({"daftar_akun_name":key, "daftar": ingroup})
+        balance = 0
+        for item in group:
+            balance+= item.debit
+            balance-= item.credit
+            ingroup.append({ "date": item.date, "note": item.note, "debit": item.debit, "credit": item.credit, "balance": balance})
+
+    return render_template("buku_besar/index.html", title="Daftar Akun", buku_besar=datas)
 
 # @server.route("/buku-besar/create", methods=['POST', 'GET'])
 # def create_buku():
